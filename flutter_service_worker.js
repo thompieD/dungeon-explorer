3463bf52796318f23bca7ac26ef1e298c160fda2
Thomas Diemel <thomas.diemel@hva.nl>
2024-10-27 20:36:00 +0100

    build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "43cac1762a48e7b64358294ca29f4932
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "6fd0a387274db3a49dcc94a11276ecd7",
+"flutter_bootstrap.js": "da986a7c9a4d27568d11a65744f45492",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
